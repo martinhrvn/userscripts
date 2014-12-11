62b5df39f9b25bb8694b6d26d137bee806385c47
Martin Harvan <martin.harvan@sk.ibm.com>
2014-12-11 14:41:14 +0100

    Bumped version

--- duolingo/lessons_progress.user.js
+++ duolingo/lessons_progress.user.js
@@ -2,7 +2,7 @@
 // @name        Duolingo Course Progress
 // @namespace   https://github.com/kane77/userscripts
 // @include     https://www.duolingo.com/*
-// @version     1.4
+// @version     1.5
 // @grant GM_getValue
 // ==/UserScript==
 function inject(f) { //Inject the script into the document
