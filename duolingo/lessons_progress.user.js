cc355a84a5fb227f6d62dda75742b7319e5bcac8
Martin Harvan <martin.harvan@sk.ibm.com>
2014-12-15 13:43:11 +0100

    Changed the progress calculation to be per language

--- duolingo/lessons_progress.user.js
+++ duolingo/lessons_progress.user.js
@@ -3,135 +3,121 @@
 // @namespace   https://github.com/kane77/userscripts
 // @include     https://www.duolingo.com/*
 // @author      Martin Harvan
-// @version     2.0
+// @version     2.1
 // @grant GM_getValue
 // ==/UserScript==
 function inject(f) { //Inject the script into the document
-    var script;
-    script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.setAttribute('name', 'lesson_stats');
-    script.textContent = '(' + f.toString() + ')(jQuery)';
-    document.head.appendChild(script);
+  var script;
+  script = document.createElement('script');
+  script.type = 'text/javascript';
+  script.setAttribute('name', 'lesson_stats');
+  script.textContent = '(' + f.toString() + ')(jQuery)';
+  document.head.appendChild(script);
 }
 inject(f);
 function f($) {
-    function duolingoStats() {
-        try {
-            langData = window.duo.user.attributes.language_data[window.duo.user.attributes.learning_language];
-            var totalSkills = langData.skills.models.length;
-            var doneSkills = 0;
-            var totalActivities = 0;
-            var doneActivities = 0;
-            for (var i = 0; i < langData.skills.models.length; i++) {
-                var skill = langData.skills.models[i];
-                totalActivities += skill.attributes.num_lessons;
-                doneActivities += skill.attributes.num_lessons - skill.attributes.missing_lessons;
-                if (skill.attributes.missing_lessons === 0) {
-                    doneSkills += 1;
-                }
-            }
-            if (langData.bonus_skills) {
-                totalSkills += langData.bonus_skills.length;
-                for (var i = 0; i < langData.bonus_skills.models.length; i++) {
-                    var skill = langData.bonus_skills.models[i];
-                    totalActivities += skill.attributes.num_lessons;
-                    doneActivities += skill.attributes.num_lessons - skill.attributes.missing_lessons;
-                    if (skill.attributes.missing_lessons === 0) {
-                        doneSkills += 1;
-                    }
-                }
-            }
-             
-            var firstTracked = localStorage['dcp_last_index'];
-            if(!firstTracked) {
-                localStorage['dcp_date_0'] = new Date();
-                localStorage['dcp_skills_0'] = doneSkills;
-                localStorage['dcp_lessons_0'] = doneActivities;
-                localStorage['dcp_last_index'] = 0;
-            } else {
-                var lastIndex = localStorage['dcp_last_index'];
-                
-                var dS = parseInt(localStorage['dcp_skills_'+lastIndex]);
-                var dL = parseInt(localStorage['dcp_lessons_'+lastIndex]);
-                
-                if(doneSkills > dS || doneActivities > dL) {
-                    var i = parseInt(lastIndex) + 1;
-                    localStorage['dcp_last_index'] = i;
-                    localStorage['dcp_skills_'+i] = doneSkills;
-                    localStorage['dcp_lessons_'+i] = doneActivities;
-                    localStorage['dcp_date_'+i] = new Date();
-                }
-                
-                
-            }
-            
-            
-            
-                
-            var lessonPercent = Math.round(doneActivities / totalActivities * 100);
-            var skillPercent = Math.round(doneSkills / totalSkills * 100);
-            
-            var skillElem = createElem(doneSkills, totalSkills, skillPercent, "Skills", "S");
-            var lessonElem = createElem(doneActivities, totalActivities, lessonPercent, "Lessons", "L");
-            
-            var stats = $('<ul class="sidebar-stats lesson-progress"></ul>');
-            skillElem.appendTo(stats);
-            lessonElem.appendTo(stats);
-            
-            var lessonDiff = (doneActivities - parseInt(localStorage['dcp_lessons_0']));
-                if(lessonDiff > 0){
-                    var timeDiff = (new Date() - new Date(localStorage['dcp_date_0']));
-                    var timePerLesson = timeDiff / lessonDiff;
-
-                    var estTime = (totalActivities - doneActivities) * timePerLesson;
-
-                    var finishDate = new Date(new Date().getTime() + estTime).toLocaleDateString();
-                    if(!$('.estimate_completion').length) {
-                        var estText = $('<span id="estimate"><strong>'+finishDate+'</strong></span>');
-                        var est = $('<li style="text-align: left; display: block; margin-top: 2px;" class="estimate_completion"><span class="icon icon-words-small estimate-icon">E</span></li>');
-                        est.append(estText);
-                        est.appendTo(stats);
-                    }
-                }
-            
-            if ($('#app').hasClass('home') && !$('.lesson-progress').length) {
-                $('.strengthen-skills-container').before(stats);
-            }
-        } catch (ex) {
-            console.log(ex);
+  function duolingoStats() {
+    try {
+      var language = window.duo.user.attributes.learning_language;
+      var langData = window.duo.user.attributes.language_data[language];
+      var totalSkills = langData.skills.models.length;
+      var doneSkills = 0;
+      var totalActivities = 0;
+      var doneActivities = 0;
+      for (var i = 0; i < langData.skills.models.length; i++) {
+        var skill = langData.skills.models[i];
+        totalActivities += skill.attributes.num_lessons;
+        doneActivities += skill.attributes.num_lessons - skill.attributes.missing_lessons;
+        if (skill.attributes.missing_lessons === 0) {
+          doneSkills += 1;
         }
+      }
+      if (langData.bonus_skills) {
+        totalSkills += langData.bonus_skills.length;
+        for (var i = 0; i < langData.bonus_skills.models.length; i++) {
+          var skill = langData.bonus_skills.models[i];
+          totalActivities += skill.attributes.num_lessons;
+          doneActivities += skill.attributes.num_lessons - skill.attributes.missing_lessons;
+          if (skill.attributes.missing_lessons === 0) {
+            doneSkills += 1;
+          }
+        }
+      }
+      storeDCPItems(new Date(), doneSkills, doneActivities, language);
+      var lessonPercent = Math.round(doneActivities / totalActivities * 100);
+      var skillPercent = Math.round(doneSkills / totalSkills * 100);
+      var skillElem = createElem(doneSkills, totalSkills, skillPercent, 'Skills', 'S');
+      var lessonElem = createElem(doneActivities, totalActivities, lessonPercent, 'Lessons', 'L');
+      var stats = $('<ul class="sidebar-stats lesson-progress"></ul>');
+      skillElem.appendTo(stats);
+      lessonElem.appendTo(stats);
+      var lessonDiff = (doneActivities - parseInt(getLSItem('lessons', language, 1)));
+      if (lessonDiff > 0) {
+        var timeDiff = (new Date() - new Date(getLSItem('date', language, 1)));
+        var timePerLesson = timeDiff / lessonDiff;
+        var estTime = (totalActivities - doneActivities) * timePerLesson;
+        var finishDate = new Date(new Date().getTime() + estTime).toLocaleDateString();
+        if (!$('.estimate_completion').length) {
+          var estText = $('<span id="estimate"><strong>' + finishDate + '</strong></span>');
+          var est = $('<li style="text-align: left; display: block; margin-top: 2px;" class="estimate_completion"><span class="icon icon-words-small estimate-icon">E</span></li>');
+          est.append(estText);
+          est.appendTo(stats);
+        }
+      }
+      if ($('#app').hasClass('home') && !$('.lesson-progress').length) {
+        $('.strengthen-skills-container').before(stats);
+      }
+    } catch (ex) {
+      console.log(ex);
     }
-    
-    function createElem(doneCount, totalCount, percent, name, icon) {
-        var elemText = $('<span id="'+ name +'_text"><strong>' + doneCount + '/' + totalCount + '</strong> '+name+'</span>'); 
-        var percentage = $('<span id="'+name+'_percent"><strong>' + percent + '%</strong></span>'); 
-        percentage.hide();
-        var elem = $('<li style="text-align: left; display: block; margin-top: 2px;" id="'+name+'_stats" title="' + percent + '%"><span class="icon icon-words-small '+name+'-icon">'+icon+'</span></li>');
-        elem.append(elemText);
-        elem.append(percentage);
-        
-        
-        
-        $('.'+name+'-icon').mouseover(function() {
-            $('#'+name+'_text').hide();
-            $('#'+name+'_percent').css('display', 'inline-block');
-            
-        })
-        .mouseout(function() {
-            $('#'+name+'_percent').hide();
-            $('#'+name+'_text').show();
-        });   
-        
-        return elem;
+  }
+  function createElem(doneCount, totalCount, percent, name, icon) {
+    var elemText = $('<span id="' + name + '_text"><strong>' + doneCount + '/' + totalCount + '</strong> ' + name + '</span>');
+    var percentage = $('<span id="' + name + '_percent"><strong>' + percent + '%</strong></span>');
+    percentage.hide();
+    var elem = $('<li style="text-align: left; display: block; margin-top: 2px;" id="' + name + '_stats" title="' + percent + '%"><span class="icon icon-words-small ' + name + '-icon">' + icon + '</span></li>');
+    elem.append(elemText);
+    elem.append(percentage);
+    $('.' + name + '-icon').mouseover(function () {
+      $('#' + name + '_text').hide();
+      $('#' + name + '_percent').css('display', 'inline-block');
+    }).mouseout(function () {
+      $('#' + name + '_percent').hide();
+      $('#' + name + '_text').show();
+    });
+    return elem;
+  }
+  $(document).ready(function () {
+    duolingoStats();
+  });
+  $(document).ajaxComplete(function () {
+    duolingoStats();
+  });
+  function getLSItem(name, language, id) {
+    if (id) {
+      return localStorage['dcp_' + language + '_' + name + '_' + id];
+    } else {
+      return localStorage['dcp_' + language + '_' + name];
     }
-    
-    $(document).ready(function () {
-        duolingoStats();
-    });
-    $(document).ajaxComplete(function () {
-        duolingoStats();
-    });
+  }
+  function setLSItem(name, language, id, value) {
+    localStorage['dcp_' + language + '_' + name + '_' + id] = value;
+  }
+  function storeDCPItems(date, skills, lessons, language) {
+    var lastIndex = getLSItem('last_index', language);
+    if (!lastIndex) {
+      lastIndex = 1;
+    } else {
+      if (lessons <= parseInt(getLSItem('lessons', language, lastIndex)) &&
+      skills <= parseInt(getLSItem('skills', language, lastIndex))) {
+        return;
+      }
+      lastIndex++;
+    }
+    setLSItem('date', language, lastIndex, date);
+    setLSItem('skills', language, lastIndex, skills);
+    setLSItem('lessons', language, lastIndex, lessons);
+    localStorage['dcp_' + language + '_last_index'] = lastIndex;
+  }
 }
-
 
